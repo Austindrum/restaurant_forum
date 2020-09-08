6ea791ffbd597d2178adcff04ffdf643a2fa8733
Austindrum <drum108306@gmail.com>
2020-09-08 16:48:53 +0800

    Add admin restaurant post & get feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,6 +9,23 @@ const adminController = {
         .then(restaurants=>{
             return res.render("admin/restaurants", { restaurants });
         })
+    },
+    createRestaurant: (req, res) => {
+        return res.render('admin/create')
+    },
+    postRestaurant: (req, res) => {
+        let { name, tel, address, opening_hours, description } = req.body;
+        if(!name){
+            req.flash("error_msg", "請輸入店名");
+            return res.redirect("/admin/restaurants/create");
+        }
+        return Restaurant.create({
+                    name, tel, address, opening_hours, description
+                })
+                .then(restaurant => {
+                    req.flash("success_msg", "餐廳建立成功");  
+                    res.redirect("/admin/restaurants");
+                })
     }
 }
 
