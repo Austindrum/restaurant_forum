0ee236d87fb3c4f6603aa7b088e334bdaa1915cb
Austindrum <drum108306@gmail.com>
2020-09-08 11:10:27 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require("express");
 const app = express();
 const db = require("./models");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+
+app.use(session({
+    secret: 'some',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+
+app.use((req, res, next)=>{
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    next();
+})
+
 
 require("./routes")(app);
 const PORT = process.env.PORT || 3000;
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,15 +7,33 @@ const userController = {
         res.render("users/signup");
     },
     signUp: (req, res) => {
-        let {name, email, password} = req.body;
-        User.create({
-            name,
-            email,
-            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-        })
-        .then(user=>{
-            return res.redirect('/signin');
-        })
+        let {name, email, password, passwordCheck} = req.body;
+        if(password !== passwordCheck){
+            req.flash("error_msg", "密碼確認錯誤");
+            return res.redirect("/signup");
+        }else{
+            User.findOne({
+                where: {
+                    email
+                }
+            })
+            .then(user=>{
+                if(user){
+                    req.flash("error_msg", "此信箱已註冊過");
+                    res.redirect("/signup");
+                }else{
+                    User.create({
+                        name,
+                        email,
+                        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+                    })
+                    .then(user=>{
+                        req.flash("success_msg", "註冊成功");
+                        return res.redirect('/signin');
+                    })
+                }
+            })
+        }
     }
 }
 module.exports = userController;
