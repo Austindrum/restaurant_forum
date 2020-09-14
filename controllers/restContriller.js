dc322ca5866cbac038a45a5580946f4d9122161c
Austindrum <drum108306@gmail.com>
2020-09-14 22:39:10 +0800

    Convert time format using moment

--- controllers/restContriller.js
+++ controllers/restContriller.js
@@ -1,7 +1,10 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const User = db.User
+const Comment = db.Comment
 const pageLimit = 10
+const moment = require("moment");
 
 const restController = {
     getRestaurants: (req, res)=>{
@@ -46,9 +49,16 @@ const restController = {
     },
     getRestaurant: (req, res) => {
         return Restaurant.findByPk(req.params.id, {
-                   include:  Category
+                   include:  [
+                       Category,
+                       { model: Comment, include: [User] }
+                    ]
                 }).then(restaurant => {
-                    return res.render('restaurant', { restaurant: restaurant.toJSON() })
+                    let data = restaurant.toJSON();
+                    data.Comments.forEach(comment=>{
+                        comment.createdAt = moment(comment.createdAt).fromNow();
+                    })
+                    return res.render('restaurant', { data })
                 })
     }
 }
